2bcc6796a114b4f10ddbf55947dd1fc6685558e2
uiraseb <uiraseeberger@ucl.br>
2025-11-29 07:34:44 -0300

    extension update

--- app/extensions.py
+++ app/extensions.py
@@ -2,10 +2,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from celery import Celery
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from celery import Celery
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -16,8 +16,8 @@ limiter = Limiter(key_func=get_remote_address)
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
+        backend=app.config.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
+        broker=app.config.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
     )
     celery.conf.update(app.config)
     return celery
